75e88723a852031be033c5ed81edfaaa84c10011
Davidvamosfull <jieli3837@gmail.com>
2025-01-13 20:49:45 +0100

    First Commit

---
+++ src/main/java/edu/fje/daw2/proyecto_prs_restjava/HelloApplication.java
@@ -0,0 +1,9 @@
+package edu.fje.daw2.proyecto_prs_restjava;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
